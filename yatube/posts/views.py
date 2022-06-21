bdb8294bca0868425cc98a9028d5e0d4c2ce0dcf
Мария Шелест <mariia.schelest@gmail.com>
2022-06-21 18:15:55 +0300

    3rd sprint project 1st review attempt

---
+++ yatube/posts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+# Create your views here.
+def index(request):
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {'posts': posts}
+    return render(request, template, context)
+
+
+def group_list(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts
+    }
+    return render(request, template, context)
